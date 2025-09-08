77a9df3925da953b43712ed57449b6c2301d2e8d
Gro0mp <denniswong342@gmail.com>
2025-09-08 15:56:33 -0400

    added to webgpu test

--- src/pages/WebGPUTest.jsx
+++ src/pages/WebGPUTest.jsx
@@ -1,17 +1,27 @@
 import {Canvas} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei";
+import {useRef} from "react";
+
+const RotatingCube = ({position, size, color}) => {
+
+    const meshRef = useRef(null);
+    return (
+      <mesh ref={meshRef}>
+        
+      </mesh>
+    );
+}
 
 export const WebGPUTest = () => {
 
-    const Cube = ({position, size, color}) => {
-
-    }
     return (
       <Canvas style={{height:'100vh',width:'100vw', display:'flex',justifyContent:'center', alignItems: 'center'}}>
-          <OrbitControls enableZoom={true} enablePan={}></OrbitControls>
+          <OrbitControls enableZoom={true} enablePan></OrbitControls>
           <directionalLight position={[1, 1, 1]} intensity={10} color={0x9CDBA6}/>
 
-          <color attach={`background`} args={``}></color>
+          <color attach={`background`} args={[`#F0F0F0`]}></color>
+
+          <RotatingCube></RotatingCube>
 
       </Canvas>
     );
