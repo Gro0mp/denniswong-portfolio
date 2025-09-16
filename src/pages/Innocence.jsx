f15c5698e8f3c6da9d569fe83cacdae1cfc1d6e3
dennisw002 <dennis.wong002@umb.edu>
2025-09-15 23:07:06 -0400

    added panels

---
+++ src/pages/Innocence.jsx
@@ -0,0 +1,12 @@
+import {Canvas, useFrame} from "@react-three/fiber";
+import {OrbitControls, Sparkles} from "@react-three/drei";
+import {FirstPersonControls} from "@react-three/drei";
+import {useRef} from "react";
+
+export const Innocence = () => {
+    return (
+        <Canvas>
+
+        </Canvas>
+    );
+}
